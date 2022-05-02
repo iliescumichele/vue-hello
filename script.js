788df2d13ce625f5949adb6c3ac12025b0317cda
iliescumichele <iliescumichele@gmail.com>
2022-05-02 23:14:28 +0200

    First VueJS, print in browser a text and image through VueJS

---
+++ script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+
+    el: '#app',
+
+    data: {
+        messaggio: 'Hello world',
+        imageUrl: 'https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj'
+    }
+})
